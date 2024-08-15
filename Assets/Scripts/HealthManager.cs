b922fa63df3a429f529351d07f90a6b128958007
3vo1ution <lefoboshielo@gmail.com>
2024-08-15 16:31:52 +0200

    HealthCode

---
+++ Assets/Scripts/HealthManager.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.PlasticSCM.Editor.WebApi;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    [SerializeField] public float MaxHealth;
+    private float currentHealth;
+    private float timeElapsed = 0f;//time since last decrease
+    public float DecreaseInterval = 15f;// health decreases every 15 seconds
+    public float HealthDecreaseAmount = 10f;//health decreases by 10 every 15 seconds
+    public float timer;
+
+    public HealthBar healthBar;
+
+
+    private void Start()
+    {
+        currentHealth = MaxHealth;
+
+        healthBar.SetMaxHealth(currentHealth);//set the max health of the player when the game starts
+
+        timer = DecreaseInterval;
+    }
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            HealthDecrease(HealthDecreaseAmount);
+            timer = DecreaseInterval;
+        }
+
+    }
+
+
+
+    public void HealthDecrease(float health)
+    {
+        currentHealth -= health;// we are subtracting from the current health which is set in the starting method
+
+        if (currentHealth < 0f) //prevent health from going lower than zero
+        {
+            currentHealth = 0f;
+
+        }
+
+        healthBar.SetSlider(currentHealth);// update slider
+
+        Debug.Log("Health:" + currentHealth);
+    }
+
+    public void HealthIncrease(float health)
+    {
+        if (currentHealth > MaxHealth)
+        {
+            currentHealth = MaxHealth;// To make sure health does not go over the max health
+        }
+
+        currentHealth += health;// we are adding to the current health which is set in the starting method
+
+        healthBar.SetSlider(currentHealth);
+
+        Debug.Log("Health:" + currentHealth);
+
+    }
+
+
+
+}
---
+++ Assets/Scripts/HealthPickup.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPickup : MonoBehaviour
+{
+
+    public float IncreaseAmount;
+    private void OnTriggerEnter(Collider other)// this function is used when a trigger collider comes in contact with another collider, the other refers to the other collider
+    {
+        if (other.CompareTag("Player"))// bool to check if the object that has collided with the health pickup has the tag player
+        {
+            other.gameObject.GetComponent<HealthManager>().HealthIncrease(IncreaseAmount);// to access the HeallthIncrease method in the health manager script
+        }
+        Destroy(gameObject);// get rid of health after player passes through it
+
+
+
+    }
+
+
+
+
+}
