859ba6ba883c25500490cb30958574a6e4b53d7e
BahleSibanda <2716324@students.wits.ac.za>
2024-08-15 15:28:11 +0200

    Health and Pick Up

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider healthSlider;
+    public float MaxHealth;
+    
+
+
+    public void SetSlider(float health )
+    {
+        healthSlider.value = health;
+    }
+
+    public void SetMaxHealth(float health )
+    {
+        healthSlider.maxValue = health;
+        SetSlider( health );
+    }
+
+   
+}
