17d22a0835ed8b4c752d916c6036aa7f262136d4
BahleSibanda <2716324@students.wits.ac.za>
2024-09-22 16:09:24 +0200

    with the script added

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public void playGame()
+    {
+        SceneManager.LoadScene("NewScene");
+
+    }
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+
+}
+
+
+
+
